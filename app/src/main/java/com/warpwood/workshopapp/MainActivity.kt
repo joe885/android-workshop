b99975763f81361b5ea73088aa23d0c3aa63abe5
Joseph Donegan <donegan.joseph@gmail.com>
2021-02-21 09:03:15 +0000

    ViewBinding

--- app/src/main/java/com/warpwood/workshopapp/MainActivity.kt
+++ app/src/main/java/com/warpwood/workshopapp/MainActivity.kt
@@ -2,11 +2,16 @@ package com.warpwood.workshopapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.warpwood.workshopapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.label.text = "My first Android app"
     }
 }
